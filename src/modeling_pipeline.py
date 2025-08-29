c1bc9553255bdb626a95d98c04536f66bb0c62b1
HoloDanger <162679391+HoloDanger@users.noreply.github.com>
2025-08-29 13:32:04 +0800

    feat: added missing prototyping notebook

--- src/modeling_pipeline.py
+++ src/modeling_pipeline.py
@@ -6,3 +6,15 @@ import time
 from tqdm import tqdm  # For a clean progress bar
 
 # Scikit-learn Imports
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+)
